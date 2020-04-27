f579af0804f16b3aff4b0e059755462999eed452
Morgan Bligh <morganbligh@icloud.com>
2020-04-26 19:34:24 -0700

    COMMIT MESSAGE

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-  
+'use strict'
+const express = require("express");
+const bodyParser = require("body-parser");
+const data = require("./data")
+const app = express();
 
-var http = require("http");
-const data = require("./data") 
-http.createServer(function(req,res) {
-  console.log(req.url)
-  const url = req.url;
-    if (url === "/") {
-        res.write("home" + "\n\n");
-        res.write(data.getAll());
-        res.end();
+let exphbs =  require("express-handlebars");
+app.engine("handlebars", exphbs({defaultLayout: false}));
+app.set("view engine", "handlebars");
+
+app.set('port', process.env.PORT || 3000);
+ 
+let people = data.getAll();
+
+app.get('/', (req,res) =>{
+console.log(req.query);
+res.render('home', {name: req.query.name, eye_colors: people});
     
-    } else if (url === "/about") {
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end('my name is Morgan Bligh and I am a student at SCC');
+    });
+    
 
-      
-    } else {
-        res.writeHead(404,{'Content-Type': 'text/plain'});
-        res.end('404 error not found');
-      
-    }
-}).listen(process.env.PORT || 3000); 
+
+app.get('/details', (req, res) => {
+console.log(req.query);
+res.render('details', {name: req.query.name, eye_colors: people});
+        
+   
+   });
+   
+ app.listen(app.get('port'), () => {
+ console.log('Express started');
+   });
